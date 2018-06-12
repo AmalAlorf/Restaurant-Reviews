82e8b7f5d90b08ca32207080d73027b615b3c4e8
Amal Alorf <amal.alorf@gmail.com>
2018-06-12 23:04:25 +0300

    fixing some issues

--- SW.js
+++ SW.js
@@ -1,6 +1,6 @@
 // Define the cache name 
 let cacheName = 'restaurantCache';
-//Define what are the URLs need to cache 
+// //Define what are the URLs need to cache 
 let urlsToCache = [
     '/',
     './index.html',
@@ -21,61 +21,57 @@ let urlsToCache = [
     './img/9.jpg',
     './img/10.jpg',
 ];
-//starting to define the life cycle of service worker with caching 
-//1. install the srvice worker
+// //starting to define the life cycle of service worker with caching 
+// //1. install the srvice worker
 self.addEventListener('install', function(event) {
     console.log('Service worker is installed');
     event.waitUntil(
         //opening the cach and adding all defined URLS
-        caches.open(cacheName).then(function(cache) {
+        caches.open(cacheName)
+        .then(function(cache) {
             console.log('cache is opened and cacahing the URLs');
             return cache.addAll(urlsToCache);
-        }))
-});
-
-//2. active the srvice worker
-self.addEventListener('activate', function(event) {
-    event.waitUntil(
-        caches.keys().then(function(cacheNames) {
-            // active of service worker will not be done until promise is done
-            return Promise.all(
-                cacheNames.filter(function(cacheName) {
-                    console.log('Service worker is activated');
-                    if (cacheName === 'restaurantCache') {
-                        console.log('Return the cache');
-                        return cacheName
-                    } else {
-                        // Delete the cache
-                        console.log('Deleting the cache')
-                        return caches.delete(cacheName);
-                    }
-                })
-            );
         })
     );
 });
-// 3. fetching steps
+// //2. active the srvice worker
+self.addEventListener('activate', function(event) {
+        console.log('Service Worker is activated');
+        event.waitUntil(
+            caches.keys().then(function(cacheNames) {
+                // active of service worker will not be done until promise is done
+                return Promise.all(
+                    cacheNames.filter(function(cacheName) {
+                        if (cacheName !== 'restaurantCache') {
+                            console.log('Deleting the cache')
+                            return caches.delete(cacheName);
+                        } else {
+                            return cacheName;
+                        }
+                    })
+                );
+            }))
+    })
+    // // 3. fetching steps
 self.addEventListener('fetch', function(event) {
-    // console.log('Service Worker is fetching', event);
     event.respondWith(
         caches.match(event.request).then(function(response) {
             if (response) {
-                // If I found the same response in the cache, return it, and also update the cache entry 
                 console.log(' Matching  with data in the cache');
-                caches.open(cacheName)
-                    .then(function(cache) {
-                        cache.add(event.request); //save the response in the cache
-                        return response;
-                    })
-            } else {
-                // If I didn't find a response in the cache fetching from the internet
-                console.log(' UnMatching with data in the cache ')
-                return fetch(event.request)
-
-                .catch(function() {
-                    console.log('Service Worker Error fetching ')
-                })
+                return response;
             }
-        })
-    )
-})
+            return fetch(event.request.clone())
+                .then(function(response) {
+                    caches.open(cacheName)
+                        .then(function(cache) {
+                            cache.put(event.request, response.clone()); //save the response in the cache
+                            console.log(' New Data Cached');
+                            return response;
+                        })
+                })
+                .catch(function() {
+                    console.log('Service Worker Error fetching the data')
+                }) //end of catch
+        }) // end of match
+    ); //end of response
+}); // end of fetching step
--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -151,17 +151,14 @@ fillBreadcrumb = (restaurant = self.restaurant) => {
  * Get a parameter by name from page URL.
  */
 getParameterByName = (name, url) => {
-        if (!url)
-            url = window.location.href;
-        name = name.replace(/[\[\]]/g, '\\$&');
-        const regex = new RegExp(`[?&]${name}(=([^&#]*)|&|#|$)`),
-            results = regex.exec(url);
-        if (!results)
-            return null;
-        if (!results[2])
-            return '';
-        return decodeURIComponent(results[2].replace(/\+/g, ' '));
-    }
-    // $("li").focus(function() {
-    //     $(this).css('color', 'red');
-    // });
+    if (!url)
+        url = window.location.href;
+    name = name.replace(/[\[\]]/g, '\\$&');
+    const regex = new RegExp(`[?&]${name}(=([^&#]*)|&|#|$)`),
+        results = regex.exec(url);
+    if (!results)
+        return null;
+    if (!results[2])
+        return '';
+    return decodeURIComponent(results[2].replace(/\+/g, ' '));
+}
